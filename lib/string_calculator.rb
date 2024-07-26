7c2163981e65429009d7d5816cf008c15bc016a6
Shipra Jha <shipra.jha@tietoevry.com>
2024-07-26 22:44:43 +0530

    Test Case 3: Multiple Numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,5 +2,7 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
     return numbers.to_i if numbers.size == 1
+
+    numbers.split(',').map(&:to_i).reduce(:+)
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -9,5 +9,9 @@ RSpec.describe StringCalculator do
     it 'returns the number itself if there is only one number' do
       expect(StringCalculator.add("1")).to eq(1)
     end
+
+    it 'returns the sum of two numbers' do
+      expect(StringCalculator.add("1,2")).to eq(3)
+    end
   end
 end
